9be1eca579e9a2a6f387d48de7252b9e31c79a8a
RachitAgrawal146 <agrawalrachit146@gmail.com>
2026-01-31 14:16:27 +0530

    Create Card.java

---
+++ CardTrick/Card.java
@@ -0,0 +1,29 @@
+package CardTrick;
+
+public class Card
+{
+    int rank;
+    char suit;
+    public Card ()//default constructor
+    {
+        rank = 1;
+        suit = 'D';
+    }
+    public Card (int rank, char suit)//parameterised constructor
+    {
+        this.rank = rank;
+        if (suit == 'D' || suit == 'C' || suit == 'H' || suit == 'S')
+            this.suit = suit;
+    }
+    void display ()//prints the rank and suit of a card
+    {
+        if (suit == 'C')
+            System.out.print (rank + "\u2663" );
+        if (suit == 'D')
+            System.out.print (rank + "\u2666" );
+        if (suit == 'H')
+            System.out.print (rank + "\u2665" );
+        if (suit == 'S')
+            System.out.print (rank + "\u2660" );
+    }
+}
